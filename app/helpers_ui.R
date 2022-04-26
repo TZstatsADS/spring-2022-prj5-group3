8ba527415808a77912ebb49de20714bb6b0f81ed
Haozhong Zheng <924763795@qq.com>
2022-04-25 22:38:13 -0400

    Add lib file for ui.R and server.R

---
+++ app/helpers_ui.R
@@ -0,0 +1,48 @@
+# Install related packages 
+#ui
+
+if (!require("shiny")) {
+  install.packages("shiny")
+}
+if (!require("rsconnect")) {
+    install.packages("rsconnect")
+}
+if (!require("leaflet")) {
+  install.packages("leaflet")
+}
+if (!require("plotly")) {
+  install.packages("plotly")
+}
+if (!require("shinyjs")) {
+  install.packages("shinyjs")
+}
+if (!require("shinyBS")) {
+  install.packages("shinyBS")
+}
+if (!require("dtplyr")) {
+  install.packages("dtplyr")
+}
+if (!require("dplyr")) {
+  install.packages("dplyr")
+}
+if (!require("DT")) {
+  install.packages("DT")
+}
+if (!require("lubridate")) {
+  install.packages("lubridate")
+}
+if (!require("stringr")) {
+  install.packages("stringr")
+}
+if (!require("rgdal")) {
+  install.packages("rgdal")
+}
+if (!require("ggplot2")) {
+  install.packages("ggplot2")
+}
+if (!require("shinythemes")) {
+  install.packages("shinythemes")
+}
+if (!require("broom")) {
+  install.packages("broom")
+}
